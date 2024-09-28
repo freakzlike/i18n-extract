7aa52b492fd18c78691bcc8d3cf81196c97b0802
freakzlike <freakzlike@gmail.com>
2024-09-28 16:24:32 +0200

    chore: upgrade dependencies (#63)

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import globals from 'globals'
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      }
+    }
+  }
+)
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
     }
   },
   test: {
+    coverage: {
+      include: [
+        'src/**/*'
+      ]
+    },
     include: [
       'tests/**/*.spec.ts'
     ]
